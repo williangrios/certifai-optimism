bd3d0a3fc056a815f1e93b85d4a1b36c6d7bda17
Willian Rios <williangrios@yahoo.com.br>
2024-03-22 20:44:12 -0300

    fix: fix bugs

--- frontend/src/app/blockchain/config.ts
+++ frontend/src/app/blockchain/config.ts
@@ -1,16 +1,15 @@
 import { http, createConfig } from 'wagmi'
-import { optimismSepolia, hardhat } from 'wagmi/chains'
+import { optimismSepolia } from 'wagmi/chains'
 import { walletConnect } from 'wagmi/connectors'
 
 const projectId = '7f989ece2fff9df5ec1515c1107d5197'
 
 export const config = createConfig({
-  chains: [optimismSepolia, hardhat],
+  chains: [optimismSepolia],
   connectors: [
     walletConnect({ projectId })
   ],
   transports: {
-    [optimismSepolia.id]: http(),
-    [hardhat.id]: http(),
+    [optimismSepolia.id]: http()
   },
 })
